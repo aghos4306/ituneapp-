e996717d1a5127765a994acd7304c49b44cf0c4f
aghoghoworkpc <aghogho.akpedeye@gmail.com>
2022-01-13 12:43:15 +0200

    created viewModel and its associate viewModelFactory class

--- app/src/main/java/com/example/mvvmitunes/viewmodel/SongsViewModel.kt
+++ app/src/main/java/com/example/mvvmitunes/viewmodel/SongsViewModel.kt
@@ -1,4 +1,27 @@
 package com.example.mvvmitunes.viewmodel
 
-class SongsViewModel {
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.mvvmitunes.model.MusicResponse
+import com.example.mvvmitunes.model.repository.Repository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class SongsViewModel(
+    val repository: Repository
+) : ViewModel() {
+    val songsLiveData = MutableLiveData<MusicResponse>()
+
+    fun getMusicResponse(search: String) {
+        CoroutineScope(Dispatchers.Main).launch {
+            val musicResponse = repository.getSongs(search)
+            if(musicResponse.isSuccessful) {
+                songsLiveData.postValue(musicResponse.body())
+            } else {
+                Log.d("response_error", musicResponse.errorBody().toString())
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/mvvmitunes/viewmodel/SongsViewModelFactory.kt
+++ app/src/main/java/com/example/mvvmitunes/viewmodel/SongsViewModelFactory.kt
@@ -1,4 +1,15 @@
 package com.example.mvvmitunes.viewmodel
 
-class SongsViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.mvvmitunes.model.repository.Repository
+import java.lang.IllegalArgumentException
+
+class SongsViewModelFactory(val repository: Repository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(SongsViewModel::class.java)) {
+            return SongsViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
